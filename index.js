b7883f5536edcb724750d1865a8ee485bc734cef
Eugene Sharygin <eush77@gmail.com>
2014-10-12 05:34:51 +0400

    Add fzip.each

--- index.js
+++ index.js
@@ -3,21 +3,30 @@
 var zip = require('lodash.zip');
 
 
-module.exports = function () {
-  if (arguments.length <= 1) {
-    return null;
-  }
+/**
+ * Make fzip that uses arrayMethod to transform the zipped result.
+ */
+var makeFzip = function (arrayMethod) {
+  return function () {
+    if (arguments.length <= 1) {
+      return null;
+    }
 
-  var collections = [].slice.call(arguments, 0, -1)
-    , mapper = [].slice.call(arguments, -1)[0];
+    var collections = [].slice.call(arguments, 0, -1)
+      , mapper = [].slice.call(arguments, -1)[0];
 
-  // If no callback passed, fall back to plain zip.
-  if (typeof mapper != 'function') {
-    collections.push(mapper);
-    return zip.apply(null, collections);
-  }
+    // If no callback passed, fall back to plain zip.
+    if (typeof mapper != 'function') {
+      collections.push(mapper);
+      return zip.apply(null, collections);
+    }
 
-  return zip.apply(null, collections).map(function (items) {
-    return mapper.apply(null, items);
-  });
+    return zip.apply(null, collections)[arrayMethod](function (items) {
+      return mapper.apply(null, items);
+    });
+  };
 };
+
+
+module.exports = makeFzip('map');
+module.exports.each = makeFzip('forEach');
--- test/test.js
+++ test/test.js
@@ -3,13 +3,27 @@
 var fzip = require('..');
 
 
-it('should zip and map', function () {
-  fzip([0, 1, 2], [2, 1, 0], function (a, b) {
-    return a + b;
-  }).should.eql([2, 2, 2]);
+describe('fzip', function () {
+  it('should zip and map', function () {
+    fzip([0, 1, 2], [2, 1, 0], function (a, b) {
+      return a + b;
+    }).should.eql([2, 2, 2]);
+  });
+
+  it('should fall back to plain zip', function () {
+    fzip([0, 1, 2], [2, 1, 0]).should.eql([[0, 2], [1, 1], [2, 0]]);
+  });
 });
 
 
-it('should fall back to plain zip', function () {
-  fzip([0, 1, 2], [2, 1, 0]).should.eql([[0, 2], [1, 1], [2, 0]]);
+describe('fzip.each', function () {
+  it('should iterate over zipped result', function () {
+    var sums = [];
+
+    (fzip.each([0, 1, 2], [2, 1, 0], function (a, b) {
+      sums.push(a + b);
+    }) == null).should.be.true;
+
+    sums.should.eql([2, 2, 2]);
+  });
 });
